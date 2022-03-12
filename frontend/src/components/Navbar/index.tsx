c8c77de71d164f9c7f1ffd346b22d88d7e852ffe
vog10 <vog10@github.com>
2022-03-12 01:47:23 -0300

    _redirects

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,23 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg'; // importando icone do github
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'; // importando icone do github
 import './styles.css';
 
-function Navbar(){
+function Navbar() {
 
-return(
-    <header>
-       <nav className="container"> 
-         <div className="dsmovie-nav-content">
-           <h1>DSMovie</h1>
-           <a href="https://github.com/Vog10">
-             <div className="dsmovie-contact-container">
-               <GithubIcon />
-             <p className="dsmovie-contact-link">/devsuperior</p>
-             </div>
-             </a>
-         </div>
-       </nav>
-     </header>
- );
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>VMovie</h1>
+                    <a href="https://github.com/Vog10">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/Vog10</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
 
 }
 
